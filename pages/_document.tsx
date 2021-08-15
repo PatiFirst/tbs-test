db617ffecf3c7d20c943c08e41483b5845853990
Patipon W <patipon888@gmail.com>
2021-08-15 23:39:35 +0700

    This is a new commit for fix error in github

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -89,13 +89,25 @@ class MyDocument extends Document {
                         type="image/png"
                         sizes="192x192"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/android-icon-192x192.png`}
+<<<<<<< HEAD
+
+
+                    />
+=======
                     /> */}
+>>>>>>> 9a9f04b5b3169f274a009c9e0bad27c056985dcc
                     <link
                         rel="icon"
                         type="image/png"
                         sizes="96x96"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/favicon-96x96.png`}
                     />
+                    <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="96x96"
+                    href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/favicon-96x96.png`}
+                     /> */}
 
                     <meta name="msapplication-TileColor" content="#ffffff" />
                     <meta
